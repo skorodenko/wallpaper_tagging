94ff5f43ba123e874fafb43563d5880c9fbe09a7
skorodenko <72010957+skorodenko@users.noreply.github.com>
2024-02-28 21:40:22 +0200

    ...

--- scripts/proc2tar.sh
+++ scripts/proc2tar.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/sh
 mkdir -p ./assets/_tar
 ROOT=./assets/_tar
-tar --verbose -chzf $ROOT/proc.tar.gz ./assets/preprocessed
+tar --verbose -chzf $ROOT/proc.tar.gz ./assets/preprocessed ./assets/coco/annotations \
+    ./assets/nus-wide/TagList1k.txt ./assets/nus-wide/TagList81.txt
