afdcc96f2c4ac547499df063bcb75305509d6b2e
skorodenko <72010957+skorodenko@users.noreply.github.com>
2024-03-09 11:01:50 +0200

    Optimal parameters for vcnn learning

--- models/vcnn.py
+++ models/vcnn.py
@@ -35,41 +35,34 @@ class VCNN(lg.LightningModule):
     def __init__(self, lr: float = ..., weight_decay: float = ...):
         super().__init__()
         self.save_hyperparameters()
-        self.backbone = nn.Sequential(
-            resnet.conv1,
-            resnet.layer1,
-            resnet.layer2,
-            resnet.layer3,
-            resnet.layer4,
-            resnet.avgpool,
-        )
+        self.conv1 = resnet.conv1
+        self.layer1 = resnet.layer1
+        self.layer2 = resnet.layer2
+        self.layer3 = resnet.layer3
+        self.layer4 = resnet.layer4
+        self.avgpool = resnet.avgpool
         self.head = nn.Linear(2048, 1000)
         self.loss_module = nn.BCEWithLogitsLoss(
-            pos_weight=torch.ones(1000),
-            reduction="none",
+            pos_weight = torch.ones([1000]),
         )
         self.activation = nn.Sigmoid()
         self.metrics = Metrics(1000)
     
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(
-            filter(lambda p: p.requires_grad, self.parameters()),
+        optimizer = torch.optim.AdamW(
+            self.parameters(),
             lr = self.hparams.lr,
             weight_decay = self.hparams.weight_decay,
+            amsgrad = True,
         )
         scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-            optimizer, 
+            optimizer,
             mode = "min",
-            factor = 0.75,
-            patience = 2,
-            threshold = 5.0,
-            min_lr = 1e-6,
+            factor = 0.5,
+            patience = 1,
+            threshold = 1e-3,
         )
-        return {
-            "optimizer": optimizer,
-            "lr_scheduler": scheduler,
-            "monitor": "val_loss",
-        }
+        return [optimizer], [scheduler]
         
     def predict(self, x: Tensor):
         x = self.forward(x)
@@ -77,8 +70,13 @@ class VCNN(lg.LightningModule):
         return x
     
     def forward(self, x: Tensor):
-        # Backbone forward
-        x = self.backbone(x)
+        # Resnet
+        x = self.conv1(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
         
         # Output layer
         x = torch.flatten(x, 1)
@@ -89,16 +87,15 @@ class VCNN(lg.LightningModule):
     def training_step(self, batch, batch_idx):
         (image, _, labels) = batch
         pred = self.forward(image)
-        loss = self.loss_module(pred, labels).max()
+        loss = self.loss_module(pred, labels)
         self.log("train_loss", loss, on_step=True, prog_bar=True)
         return loss
     
     def validation_step(self, batch, batch_idx):
         (image, _, labels) = batch
         pred = self.forward(image)
-        loss = self.loss_module(pred, labels).max()
-        pred = self.activation(pred)
-        pred = (pred > 0.45).to(torch.int64)
+        loss = self.loss_module(pred, labels)
+        pred = (self.activation(pred) > 0.5).to(torch.int64)
         labels = labels.to(torch.int64)
         self.metrics.update(pred, labels)
         self.log("val_loss", loss, prog_bar=True)
@@ -106,21 +103,20 @@ class VCNN(lg.LightningModule):
     def on_validation_epoch_end(self):
         cf1 = self.metrics.CF1()
         if1 = self.metrics.IF1()
-        self.log("C-F1", cf1, prog_bar=True)
-        self.log("I-F1", if1, prog_bar=True)
+        self.log("C_F1", cf1, prog_bar=True)
+        self.log("I_F1", if1, prog_bar=True)
         self.metrics.reset()
     
     def test_step(self, batch, batch_idx):
         (image, _, labels) = batch
         pred = self.forward(image)
-        pred = self.activation(pred)
-        pred = (pred > 0.45).to(torch.int64)
+        pred = (self.activation(pred) > 0.5).to(torch.int64)
         labels = labels.to(torch.int64)
         self.metrics.update(pred, labels)
     
     def on_test_epoch_end(self):
         cf1 = self.metrics.CF1()
         if1 = self.metrics.IF1()
-        self.log("C-F1", cf1, prog_bar=True)
-        self.log("I-F1", if1, prog_bar=True)
+        self.log("C_F1", cf1, prog_bar=True)
+        self.log("I_F1", if1, prog_bar=True)
         self.metrics.reset()
--- scripts/train/vcnn.py
+++ scripts/train/vcnn.py
@@ -1,9 +1,10 @@
 import lightning as lg
 from data import DataModule
 from pathlib import Path
+from itertools import chain
 from models.vcnn import VCNN
 from lightning.pytorch.loggers import CSVLogger
-from lightning.pytorch.callbacks import ModelCheckpoint, ModelSummary
+from lightning.pytorch.callbacks import ModelCheckpoint
 
 
 TRAINED_MODELS = Path("./assets/trained_models")
@@ -11,7 +12,7 @@ ROOT_DIR = TRAINED_MODELS / "vcnn.train"
 CKPT_PATH = TRAINED_MODELS / "vcnn.train" / "vcnn.ckpt"
 
 
-data = DataModule(batch_size = 8, prefetch_factor = 8, num_workers = 6)
+data = DataModule(batch_size = 32, prefetch_factor = 8, num_workers = 6)
 
 trainer = lg.Trainer(
     devices = 1,
@@ -19,23 +20,32 @@ trainer = lg.Trainer(
     accelerator = "gpu",
     default_root_dir = ROOT_DIR,
     logger = CSVLogger(ROOT_DIR, "logs"),
-    limit_train_batches = 0.05,
-    limit_val_batches = 0.2,
+    limit_train_batches = 0.1,
+    limit_val_batches = 0.1,
     callbacks = [
-        ModelSummary(max_depth=1),
         ModelCheckpoint(
             monitor="val_loss",
             save_top_k=4,
             dirpath=ROOT_DIR / "checkpoints",
-            filename="{epoch}---{val_loss:.2f}",
+            filename="{v_num}-{epoch}-{val_loss:.3f}-[{C_F1:.3f}, {I_F1:.3f}]",
         ),
     ],
 )
 
-model = VCNN.load_from_checkpoint(
-    CKPT_PATH,
-    lr = 0.001,
-    weight_decay = 0.1,
+model = VCNN(
+    lr = 0.1,
+    weight_decay = 0,
 )
+
+freeze_layers = [
+    model.conv1.parameters(),
+    model.layer1.parameters(),
+    model.layer2.parameters(),
+    model.layer3.parameters(),
+    #model.layer4.parameters(),
+]
+
+for param in chain(*freeze_layers):
+    param.requires_grad = False
 
 trainer.fit(model, datamodule=data)
