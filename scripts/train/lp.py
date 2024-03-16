427f1902f66608320f0678e0ca1e76c559a5972b
skorodenko <72010957+skorodenko@users.noreply.github.com>
2024-03-16 11:33:44 +0200

    Optimal lp

--- scripts/train/lp.py
+++ scripts/train/lp.py
@@ -3,29 +3,36 @@ from data import DataModule
 from pathlib import Path
 from models.lp import LP
 from lightning.pytorch.loggers import CSVLogger
-from lightning.pytorch.callbacks import ModelCheckpoint, ModelSummary
+from lightning.pytorch.callbacks import ModelCheckpoint, ModelSummary, LearningRateMonitor
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 
 
 TRAINED_MODELS = Path("./assets/trained_models")
 ROOT_DIR = TRAINED_MODELS / "lp.train"
+CKPT_PATH = TRAINED_MODELS / "lp.train" / "lp.ckpt"
 
 
 data = DataModule(batch_size = 32, prefetch_factor = 8, num_workers = 6)
+
 trainer = lg.Trainer(
     devices=1,
     max_epochs=100,
     accelerator="gpu",
     default_root_dir = ROOT_DIR,
-    limit_train_batches=0.25,
-    logger=CSVLogger(ROOT_DIR, "logs"),
+    logger=CSVLogger(ROOT_DIR, "logs", version=0),
+    limit_train_batches = 1.0,
+    limit_val_batches = 1.0,
     callbacks=[
-        ModelSummary(max_depth=2),
+        ModelSummary(2),
+        LearningRateMonitor(logging_interval = "step"),
         ModelCheckpoint(
-            monitor="val_loss",
-            save_top_k=4,
+            monitor="H_F1",
+            mode="max",
+            save_weights_only=True,
+            save_top_k=3,
             dirpath=ROOT_DIR / "checkpoints",
-            filename="{epoch}---{val_loss:.2f}",
+            save_on_train_epoch_end=True,
+            filename="{H_F1:.3f}@{v_num}@{epoch}@{val_loss:.3f}",
         ),
         EarlyStopping(
             min_delta=0.01,
@@ -34,9 +41,10 @@ trainer = lg.Trainer(
         ),
     ],
 )
+
 model = LP(
-    lr = 0.1,
-    weight_decay=1e-4,
-    momentum = 0.9,
+    lr = 0.01,
+    weight_decay=0.01,
 )
+
 trainer.fit(model, datamodule=data) 
